f82ddcab3c91be3a368fc2ff76d15a22f15f1ddb
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-12-15 00:34:43 +0300

    Improved pac ban for offline-bans support

--- lua/pac3/editor/server/bans.lua
+++ lua/pac3/editor/server/bans.lua
@@ -1,113 +1,54 @@
-local function get_bans()
-	local str = file.Read("pac_bans.txt", "DATA")
-
-	local bans = {}
-
-	if str and str ~= "" then
-		bans = util.KeyValuesToTable(str)
-	end
-
-	do -- check if this needs to be rebuilt
-		local k,v = next(bans)
-		if isstring(v) then
-			local temp = {}
-
-			for k,v in pairs(bans) do
-				temp[util.CRC("gm_" .. v .. "_gm")] = {steamid = v, name = k}
-			end
-
-			bans = temp
-		end
-	end
-
-	return bans
-end
-
 function pace.Ban(ply)
-
+	ply:SetPData("PAC_Banned", true)
 	ply:ConCommand("pac_clear_parts")
 
-	timer.Simple( 1, function() -- made it a timer because the ConCommand don't run fast enough. - Bizzclaw
-
-		net.Start("pac_submit_acknowledged")
-			net.WriteBool(false)
-			net.WriteString("You have been banned from using pac!")
-		net.Send(ply)
-
-		local bans = get_bans()
-
-		for key, data in pairs(bans) do
-			if ply:SteamID() == data.steamid then
-				bans[key] = nil
-			end
-		end
-
-		bans[ply:UniqueID()] = {steamid = ply:SteamID(), nick = ply:Nick()}
-
-		pace.Bans = bans
-
-		file.Write("pac_bans.txt", util.TableToKeyValues(bans), "DATA")
-	end)
+	net.Start("pac_submit_acknowledged")
+		net.WriteBool(false)
+		net.WriteString("You have been banned from using pac!")
+	net.Send(ply)
 end
 
 function pace.Unban(ply)
+	ply:RemovePData("PAC_Banned")
 
 	net.Start("pac_submit_acknowledged")
-		net.WriteBool(true)
+		net.WriteBool(false)
 		net.WriteString("You are now permitted to use pac!")
 	net.Send(ply)
-
-	local bans = get_bans()
-
-	for key, data in pairs(bans) do
-		if ply:SteamID() == data.steamid then
-			bans[key] = nil
-		end
-	end
-
-	pace.Bans = bans
-
-	file.Write("pac_bans.txt", util.TableToKeyValues(bans), "DATA")
-end
-
-local function GetPlayer(target)
-	for key, ply in player.Iterator() do
-		if ply:SteamID() == target or ply:UniqueID() == target or ply:Nick():lower():find(target:lower()) then
-			return ply
-		end
-	end
 end
 
 concommand.Add("pac_ban", function(ply, cmd, args)
-	if not next(args) then
-		pac.Message("Please provide one of the following: SteamID, UniqueID or Nick.")
+	if not args[1] then
+		pac.Message("Please provide steamid")
 		return
 	end
-	local target = GetPlayer(args[1])
-	if (not IsValid(ply) or ply:IsAdmin()) and target then
-		pace.Ban(target)
-		pac.Message(ply, " banned ", target, " from PAC.")
+
+	if IsValid(ply) and ply:IsAdmin() then
+		local target_ply = player.GetBySteamID(args[1])
+
+		if target_ply then
+			pace.Ban(target_ply)
+			pac.Message(ply, " banned ", target_ply, " from PAC.")
+		end
 	end
 end)
 
 concommand.Add("pac_unban", function(ply, cmd, args)
-	if not next(args) then
-		pac.Message("Please provide one of the following: SteamID, UniqueID or Nick.")
+	if not args[1] then
+		pac.Message("Please provide steamid")
 		return
 	end
-	local target = GetPlayer(args[1])
-	if (not IsValid(ply) or ply:IsAdmin()) and target then
-		pace.Unban(target)
-		pac.Message(ply, " unbanned ", target, " from PAC.")
+
+	if IsValid(ply) and ply:IsAdmin() then
+		local target_ply = player.GetBySteamID(args[1])
+
+		if target_ply then
+			pace.Unban(target_ply)
+			pac.Message(ply, " unbanned ", target_ply, " from PAC.")
+		end
 	end
 end)
 
 function pace.IsBanned(ply)
-	if not ply or not ply:IsValid() then return false end
-
-	if not pace.Bans then
-		pace.Bans = get_bans()
-	end
-
-	return pace.Bans[ply:UniqueID()] ~= nil
+	return IsValid(ply) and ply:GetPData("PAC_Banned", false)
 end
